cb605ca11a4215c9706d9f6a0489dc6ed7f2fe30
Cosalpino <mikael.juillet@cpnv.ch>
2023-05-09 16:00:58 +0200

    regex radius

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ import PlanetaryCelestialBodies from './src/assets/PlanetaryCelestialBodies.json
 import Allplanets from './src/assets/AllPlanetsData-08-05-2023.json';
 import {horizonAPI} from './src/model/dataFilter.js';
 
-horizonAPI(Allplanets.planets[1].result);
+horizonAPI(Allplanets.planets[2].result);
--- src/model/dataFilter.js
+++ src/model/dataFilter.js
@@ -10,14 +10,14 @@ export function horizonAPI(data) {
     let texteSansEspaces = data.replace(/\s+/g, "");
     console.log(texteSansEspaces);
 
-    const regexDensity = /Density \(g\/cm\^3\)      =  (\d+(\.\d{1,6})?)/;
-    const regex = /Vol\.MeanRadius\(km\)=(\d+\.\d+)/;
+    const regexDensity = /Density \(g\/cm\^3\)=(\d+(\.\d{1,6})?)/;
+    const regexRadius = /Vol\.MeanRadius\(km\)=(\d+(?:\.\d+)?)/;
+    const regex = /rot\. rate(?:,)? \(rad\/s\) =\s*(-?\d+\.\d+)/;
 
     const match = texteSansEspaces.match(regex);
     
     if (match !== null) {
-      const density = match[1];
-      console.log(density);
+      console.log(match[1]);
     } else {
       console.log("Aucune correspondance trouvée.");
     }
